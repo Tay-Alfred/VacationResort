b02502d7e0abbe2f63900b7e4b826d738247273c
Taylor <4.alfred.taylor@gmail.com>
2024-05-09 17:16:38 -0400

    js additions

---
+++ scripts/overnight.js
@@ -0,0 +1,21 @@
+"use strict";
+
+//declare variables
+const checkInDate = document.getElementById("checkIn");
+const numOfNights = document.getElementById("numOfNights");
+const queenOne = document.getElementById("queenOneBed");
+const queenTwo = document.getElementById("queenTwoBed");
+const kingBed = document.getElementById("kingBed");
+const californiaKingBed = document.getElementById("californiaKingBed");
+const militaryDiscount = document.getElementById("militaryDiscount");
+const seniorDiscount = document.getElementById("seniorDiscount");
+//input values
+
+
+//input unknown values
+
+
+//calculate
+
+
+//what do i want it to do
